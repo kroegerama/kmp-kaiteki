45b1ba06210d251c8531da59a13b125b7938b3cf
kroegerama <1519044+kroegerama@users.noreply.github.com>
2026-01-09 13:01:06 +0100

    increase version

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -3,7 +3,7 @@ object C {
     const val PROJECT_NAME = "KMP Kaiteki"
     const val PROJECT_DESCRIPTION = "A set of helper classes for modern Kotlin multiplatform projects."
 
-    const val PROJECT_VERSION = "1.0.1"
+    const val PROJECT_VERSION = "1.0.2"
     const val PROJECT_GROUP_ID = "com.kroegerama.kmp.kaiteki"
 
     const val PROJECT_URL = "https://github.com/kroegerama/kmp-kaiteki"
