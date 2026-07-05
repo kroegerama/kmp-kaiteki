c58841fcf1cc4f4cf7dd2f70fdb54187855a6367
kroegerama <1519044+kroegerama@users.noreply.github.com>
2026-07-05 14:21:56 +0200

    [compose] improve apple `Color.asUIColor()` to use correct color space

--- compose/src/appleMain/kotlin/com/kroegerama/kmp/kaiteki/compose/platform/UIColor.kt
+++ compose/src/appleMain/kotlin/com/kroegerama/kmp/kaiteki/compose/platform/UIColor.kt
@@ -1,11 +1,15 @@
 package com.kroegerama.kmp.kaiteki.compose.platform
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import platform.UIKit.UIColor
 
-public fun Color.asUIColor(): UIColor = UIColor(
-    red = red.toDouble(),
-    green = green.toDouble(),
-    blue = blue.toDouble(),
-    alpha = alpha.toDouble()
-)
+public fun Color.asUIColor(): UIColor {
+    val srgb = convert(ColorSpaces.Srgb)
+    return UIColor(
+        red = srgb.red.toDouble(),
+        green = srgb.green.toDouble(),
+        blue = srgb.blue.toDouble(),
+        alpha = srgb.alpha.toDouble()
+    )
+}
